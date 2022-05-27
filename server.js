8909e5e519264a3651759299bbf1f314ef0a9cab
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-05-27 16:35:58 -0500

    testing

--- server.js
+++ server.js
@@ -1,66 +1,59 @@
-// DEPENDENCIES
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-// const Stock = require('./models/stock.js')
-const cors = require('cors')
-// const seedData = require('./models/seed-data.js')
+//___________________
+//Dependencies
+//___________________
+const express = require('express');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+const app = express ();
+const db = mongoose.connection;
 require('dotenv').config()
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003;
+
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+);
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+//___________________
+//Middleware
+//___________________
+
+//use public folder for static assets
+app.use(express.static('public'));
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
 
-// CONNECTIONS
-// mongoose.connect('mongodb://localhost:27017/stockdata')
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, () => {
-    console.log('connected to mongo atlas...')
-})
-mongoose.connection.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+//___________________
+// Routes
+//___________________
+//localhost:3000
+app.get('/' , (req, res) => {
+  res.send('Hello World!');
+});
 
-// MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(cors())
-
-
-// RESTful CRUD ROUTES
-// app.post('/stocks/seed', (req, res) => {
-//     Stock.create(seedData, (error, createdSeedData) => {
-//         res.json(createdSeedData)
-//     })
-// })
-
-// app.post('/stocks', (req, res) => {
-//     Stock.create(req.body, (error, createdStock) => {
-//         res.json(createdStock)
-//     })
-// })
-
-app.get('/', (req, res) => {
-    res.send('hello world!')
-})
-
-// app.get('/stocks', (req, res) => {
-//     Stock.find({}, (error, foundStocks) => {
-//         res.json(foundStocks)
-//     })
-// })
-
-// app.put('/stocks/:id', (req, res) => {
-//     Stock.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedStock) => {
-//         res.json(updatedStock)
-//     })
-// })
-
-// app.delete('/stocks/:id', (req, res) => {
-//     Stock.findByIdAndRemove(req.params.id, (error, deletedStock) => {
-//         res.json(deletedStock)
-//     })
-// })
-
-
-// LISTEN
-app.listen(3000, () => {
-    console.log('listening...')
-})
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
