61dd4296cbb0265d6b688651f62429d7f96d34b5
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-05-27 19:38:08 -0500

    re-initialized server and schema files

--- server.js
+++ server.js
@@ -1,57 +1,65 @@
 // DEPENDENCIES
 const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const Stock = require('./models/stock.js')
-const cors = require('cors')
-const seedData = require('./models/seed-data.js')
+const mongoose = require ('mongoose')
+const app = express ()
+const db = mongoose.connection
+require('dotenv').config()
+const Stock = require('./controllers/stock.js')
 
-// CONNECTIONS
-mongoose.connect('mongodb://localhost:27017/stockdata')
-// mongoose.connect()
-mongoose.connection.once('open', () => {
-    console.log('connected to mongod...')
-})
+
+// PORT
+const PORT = process.env.PORT || 3003
+
+
+// DATABASE
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI)
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
 
 
 // MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-app.use(cors())
 
 
-// RESTful CRUD ROUTES
+// CRUD ROUTES
+app.get('/' , (req, res) => {
+  res.send('Hello World!')
+})
+
 app.post('/stocks/seed', (req, res) => {
-    Stock.create(seedData, (error, createdSeedData) => {
-        res.json(createdSeedData)
-    })
+  Stock.create(seedData, (error, createdSeedData) => {
+      res.json(createdSeedData)
+  })
 })
 
 app.post('/stocks', (req, res) => {
-    Stock.create(req.body, (error, createdStock) => {
-        res.json(createdStock)
-    })
+  Stock.create(req.body, (error, createdStock) => {
+      res.json(createdStock)
+  })
 })
 
 app.get('/stocks', (req, res) => {
-    Stock.find({}, (error, foundStocks) => {
-        res.json(foundStocks)
-    })
+  Stock.find({}, (error, foundStocks) => {
+      res.json(foundStocks)
+  })
 })
 
 app.put('/stocks/:id', (req, res) => {
-    Stock.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedStock) => {
-        res.json(updatedStock)
-    })
+  Stock.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedStock) => {
+      res.json(updatedStock)
+  })
 })
 
 app.delete('/stocks/:id', (req, res) => {
-    Stock.findByIdAndRemove(req.params.id, (error, deletedStock) => {
-        res.json(deletedStock)
-    })
+  Stock.findByIdAndRemove(req.params.id, (error, deletedStock) => {
+      res.json(deletedStock)
+  })
 })
 
 
-// LISTEN
-app.listen(3000, () => {
-    console.log('listening...')
-})
+// LISTENER
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
